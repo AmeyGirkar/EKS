904c664c74b148f4ef4777692c8db8ee2a891eb8
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-27 13:37:33 +0530

    test

--- variables.tf
+++ variables.tf
@@ -139,24 +139,7 @@ variable "addons" {
     version = string
   }))
   description = "List of EKS add-ons to install (leave empty if you don't have permissions)"
-  default     = [
-    {
-    name    = "vpc-cni",
-    version = "v1.20.0-eksbuild.1"
-  },
-  {
-    name    = "coredns"
-    version = "v1.12.2-eksbuild.4"
-  },
-  {
-    name    = "kube-proxy"
-    version = "v1.33.0-eksbuild.2"
-  },
-  {
-    name    = "aws-ebs-csi-driver"
-    version = "v1.46.0-eksbuild.1"
-  }
-  ]
+  default     = []
 }
 
 # ===== EKS Node Group Variables =====

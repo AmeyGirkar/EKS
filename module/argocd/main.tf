2050199e81c4e178519f3d816819f6b7c88e1629
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-27 21:53:02 +0530

    argocd loadbalancer

--- module/argocd/main.tf
+++ module/argocd/main.tf
@@ -1,16 +1,15 @@
 resource "helm_release" "argocd" {
-  name             = var.release_name
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = var.chart_version
-  namespace        = var.namespace
-  create_namespace = true
+  name       = var.release_name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = var.chart_version
+  namespace  = var.namespace
 
   # Expose the UI via a LoadBalancer (easy for a demo).  Change to Ingress if you prefer.
-  # set = [{
-  #   name  = "server.service.type"
-  #   value = "LoadBalancer"
-  # }]
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
 
   # Pass any custom values the user supplies (YAML or map)
   # values = var.custom_values

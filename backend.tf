bfbb27872d009d49a06632e013ee54848885c706
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-27 15:13:58 +0530

    test

--- backend.tf
+++ backend.tf
@@ -28,7 +28,7 @@ provider "aws" {
 
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = module.eks.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority)
     token                  = data.aws_eks_cluster_auth.eks.token

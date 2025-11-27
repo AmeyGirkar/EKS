9fa7b6a51028bcbb7cb47800bd9c9ed6e99a015f
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-27 14:24:20 +0530

    helm auth

--- backend.tf
+++ backend.tf
@@ -24,3 +24,15 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.eks_cluster_name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
